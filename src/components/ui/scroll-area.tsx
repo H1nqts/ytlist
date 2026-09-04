151c1eddf94d80d6d952d1aa836de9b1ebd7f44a
H1nqts <tkaqua27@gmail.com>
2026-09-04 10:29:56 +0900

    fix: constrain scroll area content to the viewport width

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -20,7 +20,7 @@ function ScrollArea({
       <ScrollAreaPrimitive.Viewport
         ref={viewportRef}
         data-slot="scroll-area-viewport"
-        className="size-full overscroll-contain rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1"
+        className="size-full overscroll-contain rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 [&>div]:block!"
       >
         {children}
       </ScrollAreaPrimitive.Viewport>
